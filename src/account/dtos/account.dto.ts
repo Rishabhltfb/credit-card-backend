adba8c9ccc3820af9b90829d3b0d07c95f52ef1f
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2023-08-21 01:03:35 +0530

    feat[validation]: add dto validations

--- src/account/dtos/account.dto.ts
+++ src/account/dtos/account.dto.ts
@@ -1,10 +1,25 @@
+import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
+
 export class CreateAccountDto {
+  @IsNotEmpty()
+  @IsString()
   customerId: string;
+
+  @IsNumber()
+  @Min(1)
   accountLimit: number;
+
+  @IsNumber()
+  @Min(1)
   perTransactionLimit: number;
 }
 
 export class UpdateAccountDto {
+  @IsNumber()
+  @Min(1)
   newAccountLimit: number;
+
+  @IsNumber()
+  @Min(1)
   newPerTransactionLimit: number;
 }
--- src/offer/dtos/offer.dto.ts
+++ src/offer/dtos/offer.dto.ts
@@ -1,12 +1,31 @@
+import {
+  IsDate,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Min,
+} from 'class-validator';
 import { LimitTypeEnum, OfferStatusEnum } from '../enum/offer.enum';
 
 export class CreateOfferDto {
+  @IsString()
+  @IsNotEmpty()
   accountId: string;
+
+  @IsEnum(LimitTypeEnum)
   limitType: LimitTypeEnum;
+
+  @IsNumber()
+  @Min(1)
   newLimit: number;
+
   offerActivationTime?: Date;
+
+  @IsDate()
   offerExpiryTime: Date;
 }
 export class UpdateOfferStatusDto {
+  @IsEnum(OfferStatusEnum)
   status: OfferStatusEnum;
 }
