450f375c38b33acdd64fbd2a484588380593d080
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2023-08-20 22:53:33 +0530

    feat[account]: add fetch account by id controller function

--- src/account/controller/account.controller.ts
+++ src/account/controller/account.controller.ts
@@ -1,18 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Param, Post } from '@nestjs/common';
 import { CustomError } from 'src/error/error.interface';
 import { CreateAccountDto } from '../dtos/account.dto';
 import { AccountService } from '../services/account.service';
+import { AccountEntity } from '../entities/account.entity';
 
 @Controller('account')
 export class AccountController {
+  private logger = new Logger(AccountController.name);
   constructor(private accountService: AccountService) {}
 
   @Post()
   async createAccount(
     @Body() createAccountDto: CreateAccountDto,
-  ): Promise<boolean | CustomError> {
-    const res = await this.accountService.createAccount(createAccountDto);
-    if (res instanceof CustomError) return res;
-    return true;
+  ): Promise<AccountEntity | CustomError> {
+    const account = await this.accountService.createAccount(createAccountDto);
+    return account;
+  }
+
+  @Get('fetch/:accountId')
+  getAccountById(
+    @Param('accountId') accountId: string,
+  ): Promise<AccountEntity | CustomError> {
+    this.logger.debug(accountId);
+    return this.accountService.fetchAccountById(accountId);
   }
 }
