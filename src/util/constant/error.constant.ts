c2ee0645a9ccf4881d7e6da71ecf742c88ecbb59
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2023-08-20 22:31:32 +0530

    chore: add error constants

---
+++ src/util/constant/error.constant.ts
@@ -0,0 +1,9 @@
+export class ERROR_STATUS_CONSTANTS {
+  static VALIDATION_FAILED = 'VALIDATION_FAILED';
+  static CUSTOMER_NOT_FOUND = 'CUSTOMER_NOT_FOUND';
+}
+export class ERROR_MSG_CONSTANTS {
+  static ACCOUNT_CREATION_FAILED = 'Account creation checks failed!';
+  static CUSTOMER_NOT_FOUND =
+    'Account creation failed because customer not found!';
+}
